aba1899bb9268df8dd856cd2efe22caa5ff8ffc1
אלון עמרם <elonamrammm1234@gmail.com>
2025-01-14 00:27:45 +0200

    Added .env file for database configuration, added .env file to .gitignore .  created db.js file for SQL connection using mysql2 with environment variables.

---
+++ server/db.js
@@ -0,0 +1,23 @@
+//import var from env file
+require('dotenv').config();
+
+//import mysql2 this allows work with sql
+const mysql=require('mysql2');
+
+//create pool for connect to DB
+const pool=mysql.createPool({
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_NAME,
+    port:process.env.DB_PORT,
+    waitForConnections:true,
+    connectionLimit:10,
+    queueLimit:0
+});
+
+//working with promise
+const db=pool.promise();
+
+//export connect db for uses
+module.exports=db;
