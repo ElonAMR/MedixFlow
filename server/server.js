40c5685bfe1944a23f24c20f5b440f267b9634b1
אלון עמרם <elonamrammm1234@gmail.com>
2025-01-13 18:07:10 +0200

    - create basic server setup with Express. - Added a basic server.js file. - Configured Express to handle a simple GET request on the root route. - Included server listening on the specified PORT.

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const cors=require('cors');
+//const dotenv=require('dotenv');
+
+//dotenv.config();
+
+
+//create object of express
+const app=express();
+
+
+const PORT=5000;
+
+
+//for Managing customer requests
+app.use(cors());
+
+
+//create basic testing endpoint for customer requests
+app.get('/',(req,res)=>{
+   res.send('Hello Main Page MedixFlow');
+});
+
+
+//
+app.listen(PORT,()=> {
+    console.log(`Server running on  http://localhost:${PORT}`);
+});
