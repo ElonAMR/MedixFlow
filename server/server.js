03fb726e7068a6e826cd016fbe14a1eeed9c41ae
אלון עמרם <elonamrammm1234@gmail.com>
2025-01-14 12:01:00 +0200

    Added endpoint for testing database connection using '/db-test' route, handles success and error scenarios.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const cors=require('cors');
 
+const db=require('./db');
 
 //create object of express
 const app=express();
@@ -19,7 +20,23 @@ app.get('/',(req,res)=>{
 });
 
 
-//
+
+
+app.get('/test-db',async (req,res)=>{
+    try {
+        await db.query('SELECT 1');
+        console.log('connected to database');
+        res.send('Database connection successful!');
+    }catch (err){
+        console.error('Failed to connect to the database:', err.message);
+        res.status(500).send('Database connection failed')
+    }
+});
+
+
+
+
+//wait for request on port x
 app.listen(PORT,()=> {
     console.log(`Server running on  http://localhost:${PORT}`);
 });
